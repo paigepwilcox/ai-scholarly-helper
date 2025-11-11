7e827eb7609c04536c74239deca9d09a3e7ecbc2
paigepwilcox <ph.paigee@gmail.com>
2025-11-11 10:27:14 -1000

    testing

--- ai_scholarly_helper_backend/ai_scholarly_helper_app/views.py
+++ ai_scholarly_helper_backend/ai_scholarly_helper_app/views.py
@@ -15,65 +15,7 @@ json_combined = {
             ]
         }
 
-# Create your views here.
-class Analyze(APIView):
-    def post(self, request):
-        abstract_text = request.data.get("section", "")
-
-        if not abstract_text:
-            return Response({"error": "No abstract text present"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        prompt = f"Define all specialized language and methodology found in the text below and return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_combined} text: {abstract_text}"
-
-        try: 
-            output = prompt_chatgpt(prompt)
-            parsed_output = json.loads(output)
-            return Response(parsed_output)
-        except Exception as e:
-            return Response({ "error": str(e) }, status=status.HTTP_400_BAD_REQUEST)
-    
-
-# Speacilized Language / Terminology / Verbage 
-class AnalyzeLanguage(APIView):
-    def post(self, requet):
-        abstract_text = requet.data.get("section", "")
-
-        if not abstract_text:
-            return Response({ "Error in AnalyzeLanguage View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # placeholderm returns only the terms 
-        return Response({
-            "terms": [
-                {"term": "mRNA", "definition": "Messenger RNA, carries genetic info from DNA."},
-                {"term": "ELISA", "definition": "A lab technique to detect antigens or antibodies."}
-            ]
-        })
-    
-class AnalyzeMethodology(APIView):
-    def post(self, requet):
-        abstract_text = requet.data.get("section", "")
-
-        if not abstract_text:
-            return Response({ "Error in AnalyzeLanguage View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        # placeholderm returns only the terms 
-        return Response({
-            "methodologies": [
-                { "methodology": "surveys and questionnaires", 
-                "definition" : "This study used quantitative PCR to amplify mRNA sequences."},
-                {"methodology": "clinical trials", 
-                "definition" : "A systematic process to find out the safety and efficacy of an intervention (like a drug or device) in treating/preventing/diagnosing a disease or medical condition."}
-            ]
-        })
-
-class AnalyzeWithQuestions(APIView):
-    def post(self, requet):
-        abstract_text = requet.data.get("section", "")
-
-        if not abstract_text:
-            return Response({ "Error in AnalyzeLanguage View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        return Response({
+json_combined_questions = {
             "terms": [
                 {"term": "mRNA", 
                 "definition": "Messenger RNA, carries genetic info from DNA.",
@@ -98,4 +40,83 @@ class AnalyzeWithQuestions(APIView):
                 "definition" : "A study design where participants are randomly assigned to either the treatment or control group to reduce bias and establish causality.",
                 "questions": ["What are the benefits of randomization in this study's context?", "How was blinding handled, and why is it important?"]}
             ]
-        })
+        }
+
+json_questions = {
+            "questions": [
+                {"question": "What is the purpose of MRNA in the process?", "answer": "Carries genetic info from DNA."},
+                {"question": "What is the purpose of DNA in the process?", "answer": "Gives the instructions to the body."}
+            ]
+        }
+
+# Create your views here.
+class Analyze(APIView):
+    def post(self, request):
+        abstract_text = request.data.get("section", "")
+
+        if not abstract_text:
+            return Response({"error": "No abstract text present"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        prompt = f"Define all specialized language and methodology found in the text below and return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_combined} text: {abstract_text}"
+
+        try: 
+            output = prompt_chatgpt(prompt)
+            parsed_output = json.loads(output)
+            return Response(parsed_output)
+        except Exception as e:
+            return Response({ "Error in Analyze": str(e) }, status=status.HTTP_400_BAD_REQUEST)
+    
+
+# Speacilized Language / Terminology / Verbage 
+class AnalyzeLanguage(APIView):
+    def post(self, requet):
+        abstract_text = requet.data.get("section", "")
+
+        if not abstract_text:
+            return Response({ "Error in AnalyzeLanguage View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        prompt = f"Define all specialized language found in the text below and return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_combined} text: {abstract_text}"
+
+        try:
+            output = prompt_chatgpt(prompt)
+            parsed_output = json.loads(output)
+            return Response(parsed_output)
+        except Exception as error:
+            return Response({ "Error in AnalyzeLanguage": str(error) }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AnalyzeMethodology(APIView):
+    def post(self, requet):
+        abstract_text = requet.data.get("section", "")
+
+        if not abstract_text:
+            return Response({ "Error in AnalyzeLanguage View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        prompt = f"Define all methodologies found in the text below and return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_combined} text: {abstract_text}"
+
+        try:
+            output = prompt_chatgpt(prompt)
+            parsed_output = json.loads(output)
+            return Response(parsed_output)
+        except Exception as error:
+            return Response({ "Error in AnalyzeMethodology": str(error) }, status=status.HTTP_400_BAD_REQUEST)
+
+
+class AnalyzeWithQuestions(APIView):
+    def post(self, requet):
+        abstract_text = requet.data.get("section", "")
+
+        if not abstract_text:
+            return Response({ "Error in AnalyzeLanguage View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
+
+        prompt = f"Define all specialized language and methodology found in the text below, for each term found create two active reading questions that will test my knowledge. Return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_combined_questions} text: {abstract_text}"
+
+class ActiveReadingQuestions(APIView):
+    def post(self, request):
+        abstract_text = request.data.get("section", "")
+
+        if not abstract_text:
+            return Response({ "Error in ActiveReadingQuestions View": "No text present" }, status=status.HTTP_400_BAD_REQUEST)
+        
+        prompt = f"Conceptualize the text below and create active reading questions that will test my knowledge. Return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_combined_questions} text: {abstract_text}"
+        
