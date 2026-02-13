1e8c4f88118d0d61264d14b195822c74b4da1490
paigepwilcox <ph.paigee@gmail.com>
2026-02-13 12:29:59 -1000

    updated prompt and data structure in views

--- ai_scholarly_helper_backend/ai_scholarly_helper_app/views.py
+++ ai_scholarly_helper_backend/ai_scholarly_helper_app/views.py
@@ -7,8 +7,10 @@ from .chatgpt_helpers import prompt_chatgpt
 
 json_terms = {
     "terms": [
-                {"term": "mRNA", "definition": "Messenger RNA, carries genetic info from DNA."}, {"term": "RNA", "definition": "RNA, the building blocks and in this context carries genetic info from DNA."}
+                {"term": "", "definition": ""}, 
+                {"term": "RNA", "definition": ""}
             ],
+    "mode": "one"
 }
 
 json_methodologies = {
@@ -17,54 +19,34 @@ json_methodologies = {
                 "definition" : "This study used quantitative PCR to amplify mRNA sequences."},
                 { "methodology": "double bline placebo", 
                 "definition" : "This study method is the golden standard used to test drugs. It takes two control groups of people and gives one group the drug and the other a sugar pill of some sort to see if the effects of the drug are truly due to the drug or due to our own mind playing tricks over us."},
-            ]
+            ],
+    "mode": "one"
 }
 
+# normalized json structure 
 json_combined_questions = {
             "terms": [
-                {"term": "mRNA", 
-                "definition": "Messenger RNA, carries genetic info from DNA.",
-                "questions": [
-                    "What is the role of mRNA in protein synthesis?",
-                    "How does mRNA differ from tRNA?"],
-                "answers": [
-                    "The rol of mRNA is this",
-                    "mRNA differs from tRNA by way of"
-                ]
-                },
-                {"term": "ELISA",
-                "definition": "Stands for Enzyme-Linked Immunosorbent Assay. A lab technique to detect antigens or antibodies.",
-                "questions": [
-                    "What is this technique used for?",
-                    "What does ELISA stand for?"],
-                "answers": [
-                    "This technique is used for this",
-                    "ELISA stands for this"
-                ]
-                }
-            ],
+                {"term": "", 
+                "definition": "",},
+                {"term": "",
+                "definition": "",}
+                ],
             "methodologies": [
                 {
-                    "methodology": "cohort study", 
-                    "definition" : "An observational study that follows a group (cohort) of individuals over time to assess outcomes based on exposures or characteristics.",
-                    "questions": ["What criteria were used to define the cohort in this study?", 
-                                  "What confounding variables might affect the outcome?"],
-                    "answers": [
-                        "This technique is used for this",
-                        "ELISA stands for this"
-                    ]
+                    "methodology": "", 
+                    "definition" : "",
                 },
-                {"methodology": "randomized controlled trial", 
-                "definition" : "A study design where participants are randomly assigned to either the treatment or control group to reduce bias and establish causality.",
-                "questions": [
-                        "What are the benefits of randomization in this study's context?", 
-                        "How was blinding handled, and why is it important?"],
-                "answers": [
-                        "This technique is used for this",
-                        "ELISA stands for this"
-                    ]
-                }
-            ],
+                {"methodology": "", 
+                "definition" : ""}
+                ],
+            "questions": [ {"placeholder": "", 
+                            "question": "", 
+                            "answer": ""}, 
+                            {"placeholder": "", 
+                             "question": "",
+                             "answer": ""}
+                ],
+            "mode": "all"
         }
 
 json_questions = {
@@ -76,7 +58,8 @@ json_questions = {
                 {"placeholder": "DNA", 
                  "question": ["What is the purpose of DNA in the process?", "If DNA wasnt part of this process what would happen?"], 
                  "answers": ["Gives the instructions to the body.", "The process would break down during stage two."]}
-            ]
+            ],
+            "mode": "one"
         }
 
 
@@ -89,10 +72,14 @@ class AnalyzeWithQuestions(APIView):
         if not abstract_text:
             return Response({ "Error in AnalyzeLanguage View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
 
-        prompt = f"Define all specialized language and methodology found in the text below and for each term found create two active reading questions that will test my knowledge. Return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_combined_questions} text: {abstract_text}"
+        prompt = f"Define all specialized language and methodology found in the text below. Create 2-4 active reading questions with respective answers based on the text below to promote reading comprehension. Return data in the canonical structure provided here, do not include markdown, extra white space, or new line syntax. data structure: {json_combined_questions} text: {abstract_text}"
 
         try:
+            print("Calling prompt_chatgpt...")
             output = prompt_chatgpt(prompt)
+            print("RAW MODEL OUTPUT: ")
+            print(output)
+
             parsed_output = json.loads(output)
             return Response(parsed_output)
         except Exception as error:
@@ -106,7 +93,7 @@ class AnalyzeLanguage(APIView):
         if not abstract_text:
             return Response({ "Error in AnalyzeLanguage View": "No abstract text found"}, status=status.HTTP_400_BAD_REQUEST)
         
-        prompt = f"Define all specialized language found in the text below and return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_terms} text: {abstract_text}"
+        prompt = f"Define all specialized language found in the text below. Return data in the canonical structure provided here, do not include markdown, extra white space, or new line syntax. data structure: {json_terms} text: {abstract_text}"
 
         try:
             output = prompt_chatgpt(prompt)
@@ -123,7 +110,7 @@ class AnalyzeMethodology(APIView):
         if not abstract_text:
             return Response({ "Error in AnalyzeMethodology View": "No text present"}, status=status.HTTP_400_BAD_REQUEST)
         
-        prompt = f"Define all methodologies found in the text below and return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_methodologies} text: {abstract_text}"
+        prompt = f"Define all methodologies found in the text below. Return data in the canonical structure provided here, do not include markdown, extra white space, or new line syntax. data structure: {json_methodologies} text: {abstract_text}"
 
         try:
             output = prompt_chatgpt(prompt)
@@ -140,7 +127,7 @@ class ActiveReadingQuestions(APIView):
         if not abstract_text:
             return Response({ "Error in ActiveReadingQuestions View": "No text present" }, status=status.HTTP_400_BAD_REQUEST)
         
-        prompt = f"Conceptualize the text below and create active reading questions that will test my knowledge. Use a word from the text as a way to placehold where the question should be asked. Return valid JSON only, do not include markdown do not include extra white space or new line syntax. json format: {json_questions} text: {abstract_text}"
+        prompt = f"Conceptualize the text below and create active reading questions that will test my knowledge. Use a word from the text as a way to placehold where the question should be asked. Return data in the canonical structure provided here, do not include markdown, extra white space, or new line syntax. data structure: {json_questions} text: {abstract_text}"
         try:
             output = prompt_chatgpt(prompt)
             parsed_output = json.loads(output)
